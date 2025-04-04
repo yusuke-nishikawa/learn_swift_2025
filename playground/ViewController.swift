f4960b733cb97b0c9975119c7cfece83b99212e7
Yusuke Nishikawa <yusuke.nishikawa@metamoji.com>
2025-04-04 15:26:18 +0900

    Initial Commit

---
+++ playground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  playground
+//
+//  Created by 西川裕介 on 2025/04/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ playgroundTests/playgroundTests.swift
@@ -0,0 +1,17 @@
+//
+//  playgroundTests.swift
+//  playgroundTests
+//
+//  Created by 西川裕介 on 2025/04/04.
+//
+
+import Testing
+@testable import playground
+
+struct playgroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
